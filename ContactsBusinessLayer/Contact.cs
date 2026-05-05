155354886196abe741ae760ccb59a1ce9dc3babf
ussiiefsayed-ctrl <ussiiefsayed@gmail.com>
2026-05-05 16:35:06 +0300

    Add files via upload

---
+++ ContactsBusinessLayer/Contact.cs
@@ -0,0 +1,129 @@
+﻿using ContactsDataAccessLayer;
+using System;
+using System.Data;
+
+namespace ContactBusinessLayer
+{
+
+    public class clsContacts
+    {
+
+        public enum enMode { AddNew = 0 , Update = 1}
+        public enMode Mode = enMode.AddNew; 
+        
+        public int ID{ set; get; }
+        public string FirstName{ set; get; }
+        public string LastName { set; get; }
+        public string Email { set;get; }
+
+        public string Phone { set; get; }
+        public string Address { set; get; }
+        public DateTime DateOfBirth { set; get; }
+        public string ImagePath { set; get; }
+        public int CountryID { set; get; }
+        public clsContacts()
+        {
+            this.ID = -1;
+            this.FirstName = "";
+            this.LastName = "";
+            this.Email = "";
+            this.Phone = "";
+            this.Address = "";
+            this.DateOfBirth = DateTime.Now;
+            this.CountryID = -1;
+            this.ImagePath = "";
+
+            Mode = enMode.AddNew;
+        }
+        private clsContacts(int Id ,string FirstName,string LastName,string Email,string Phone,string Address,
+            DateTime DateOfBirth,int CountryID,string ImagePath)
+        {
+            this.ID = Id;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Email = Email;
+            this.Phone = Phone;
+            this.Address = Address;
+            this.DateOfBirth = DateOfBirth;
+            this.CountryID = CountryID;
+            this.ImagePath = ImagePath;
+
+            Mode = enMode.Update;
+        }
+        static public clsContacts Find(int Id)
+        {
+            string FirstName = "", LastName = "", Email = "", Phone = "", Address = "",ImagePath = "";
+            DateTime BirthOfDate = DateTime.Now;
+            int CountryID = 0;
+
+            if(clsContactsDataAccess.GetContactInfoById(Id,ref FirstName, ref LastName,ref Email
+                ,ref Phone,ref Address,ref BirthOfDate,ref CountryID,ref ImagePath))
+            {
+                return new clsContacts(Id, FirstName, LastName, Email, Phone
+                    , Address, BirthOfDate, CountryID, ImagePath);
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+        private bool _AddNewContact()
+        {
+            //Call Data Access Layer
+            
+            this.ID = clsContactsDataAccess.AddNewContact(this.FirstName,this.LastName,this.Email,
+                this.Phone,this.Address,this.DateOfBirth,this.CountryID,this.ImagePath);
+
+            return (ID != -1);
+        }
+        private bool _UpdateContact()
+        {
+
+            //Call Data Access Layer
+            
+            return clsContactsDataAccess.UpdateContact(this.ID,this.FirstName,this.LastName,this.Email,this.Phone,
+                   this.Address,this.DateOfBirth,this.CountryID,this.ImagePath);
+
+        }
+        public bool Save()
+        {
+           
+            switch (Mode)
+            {
+                case enMode.AddNew:
+
+                if(_AddNewContact())
+                {
+                        Mode = enMode.Update;
+                        return true;
+                }
+                else
+                {
+                        return false;
+                }
+
+                case enMode.Update:
+
+                return _UpdateContact();
+
+            }
+            return false;   
+        }
+        public static bool DeleteContact(int ContactID)
+        {
+            return clsContactsDataAccess.DeleteContact(ContactID);
+        }
+        public static bool IsContactExist(int ContactID)
+        {
+            return clsContactsDataAccess.IsContactExist(ContactID);
+        }
+        public static DataTable GetAllContacts()
+        {
+
+            return clsContactsDataAccess.GetAllContacts();
+        }
+
+    }
+
+}
---
+++ ContactsBusinessLayer/Country.cs
@@ -0,0 +1,139 @@
+﻿using ContactsDataAccessLayer;
+using System;
+using System.Data;
+
+namespace ContactBusinessLayer
+{
+    public class clsCountry
+    {
+        public enum enMode { AddNew = 0, Update = 1 }
+        enMode Mode = enMode.AddNew;
+
+        public int CountryID { get; set; }
+        public string CountryName { get; set; }
+        public string Code { get; set; } 
+        public string PhoneCode { get; set; }
+
+        public clsCountry()
+        {
+            this.CountryID = -1;
+            this.CountryName = "";
+            this.Code = "";
+            this.PhoneCode = "";
+
+            Mode = enMode.AddNew;
+
+        }
+
+        private clsCountry(int countryID, string countryName,string Code,string PhoneCode)
+        {
+            this.CountryID = countryID;
+            this.CountryName = countryName;
+            this.Code = Code;
+            this.PhoneCode = PhoneCode;
+
+            Mode = enMode.Update;
+        }
+
+        private bool _AddNewCountry()
+        {
+            //Call Data Access Layer
+
+            this.CountryID = clsCountryData.AddNewCountry(this.CountryName,this.Code,this.PhoneCode);
+
+            return (this.CountryID != -1);
+        }
+
+        private bool _UpdateCountry()
+        {
+            //Call DataAccess Layer
+            return clsCountryData.UpdateCountry(this.CountryID, this.CountryName,this.Code,this.PhoneCode);
+        }
+
+        public static clsCountry Find(int CountryID)
+        {
+            string CountryName = "";
+            string Code = "";
+            string PhoneCode = "";
+
+            if (clsCountryData.GetCountryInfoByID(CountryID, ref CountryName,ref Code,ref PhoneCode))
+            {
+                return new clsCountry(CountryID, CountryName,Code,PhoneCode);
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        public static clsCountry Find(string CountryName)
+        {
+            int CountryID = -1;
+            string Code = "";
+            string PhoneCode = "";
+
+            if (clsCountryData.GetCountryInfoByName(CountryName, ref CountryID,ref Code,ref PhoneCode))
+            {
+                return new clsCountry(CountryID, CountryName,Code,PhoneCode);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewCountry())
+                    {
+
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+
+                    return _UpdateCountry();
+
+            }
+
+
+
+
+            return false;
+        }
+
+
+        public static DataTable GetAllCountries()
+        {
+            return clsCountryData.GetAllCountries();
+        }
+
+        public static bool DeleteCountry(int CountryID)
+        {
+            return clsCountryData.DeleteCountry(CountryID);
+        }
+
+        public static bool IsCountryExist(int CountryID)
+        {
+            return clsCountryData.IsCountryExist(CountryID);
+        }
+
+        public static bool IsCountryExist(string CountryName)
+        {
+            return clsCountryData.IsCountryExist(CountryName);
+        }
+
+
+    }
+
+
+}
